f9be26fcc1bc28978da2871cc34cd108fed52a92
Saoud Panezai <saoud.pro@gmail.com>
2026-03-25 23:34:03 +0500

    fix: empty migrations index to unblock build

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_migrateToRichText from './migrateToRichText';
-
-export const migrations = [
-  {
-    up: migration_migrateToRichText.up,
-    down: migration_migrateToRichText.down,
-    name: 'migrateToRichText'
-  },
-];
+export const migrations = [];
